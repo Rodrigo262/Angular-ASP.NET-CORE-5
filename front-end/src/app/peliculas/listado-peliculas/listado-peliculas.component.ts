2c7826362d59fa8ef5f2334fc0f2841e6412512a
Rodrigo <rodrigodhv@gmail.com>
2025-05-13 18:10:28 +0200

    Sección 1

---
+++ front-end/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-peliculas',
+  templateUrl: './listado-peliculas.component.html',
+  styleUrl: './listado-peliculas.component.css',
+})
+export class ListadoPeliculasComponent implements OnInit {
+  @Input()
+  peliculas: any;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
